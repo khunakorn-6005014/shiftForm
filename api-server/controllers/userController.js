2157e376b3b36570148a4d6aea4fc2623b54f4f2
yihwan.kim <rygd7tr44f1ikt129rgmmh97192x7f@bots.bitbucket.org>
2025-07-23 12:35:30 -0700

    Get User by Id

--- api-server/controllers/userController.js
+++ api-server/controllers/userController.js
@@ -2,6 +2,23 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user.js');
 const { signToken } = require('../utils/auth.js');
 
+exports.getUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await User.findById(id).select('-password');
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.error('Error fetching user by ID:', err.message);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
 // Login
 exports.login = async (req, res) => {
   try {
--- api-server/routes/userRoutes.js
+++ api-server/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { createUser, login } = require('../controllers/userController');
+const { createUser, login, getUserById } = require('../controllers/userController');
 
 router.post('/', createUser);
 router.post('/login', login);
+router.get('/:id', getUserById);
 
 module.exports = router;
