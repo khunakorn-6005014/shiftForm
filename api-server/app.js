f2a4b1962134506e6d00748a0ff2f04a60daebb5
yihwan.kim <rygd7tr44f1ikt129rgmmh97192x7f@bots.bitbucket.org>
2025-07-23 18:56:57 -0700

    Create Comment with first setup for comment ( model, routes, controller, and base endpoint )

--- api-server/app.js
+++ api-server/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config.js');
 const userRoutes = require('./routes/userRoutes.js');
+const commentRoutes = require('./routes/commentRoutes.js')
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,7 @@ app.use(express.json());
 connectDB();
 
 app.use('/api/user', userRoutes);
+app.use('/api/comment', commentRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
