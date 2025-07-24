f2b9f98a7572c391d467774e3b67cbb26ebb3ca0
yihwan.kim <rygd7tr44f1ikt129rgmmh97192x7f@bots.bitbucket.org>
2025-07-24 02:33:19 -0700

    Get Comment By Id

--- api-server/controllers/commentController.js
+++ api-server/controllers/commentController.js
@@ -1,5 +1,24 @@
 const Comment = require('../models/comment.js');
 
+// Get Comment by Id
+const getCommentById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const comment = await Comment.findById(id);
+
+    if (!comment) {
+      return res.status(404).json({ message: 'Comment not found' });
+    }
+
+    res.status(200).json(comment);
+  } catch (err) {
+    console.error('Error fetching comment by ID:', err.message);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+// Create Comment
 const createComment = async (req, res) => {
     try {
         const { description } = req.body.comment;
@@ -21,4 +40,7 @@ const createComment = async (req, res) => {
     }
 };
 
-module.exports = { createComment };
+module.exports = { 
+    createComment, 
+    getCommentById
+};
